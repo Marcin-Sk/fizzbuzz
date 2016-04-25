720976104cd277e22876a7b1a670d50d4794e1cf
Marcin-Sk <suchszy@o2.pl>
2016-04-25 18:39:15 +0200

    Added files via upload

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string A = "";
+
+                if (i % 3 == 0)
+                {
+                    A += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    A += ((A != "" )? " " : "") + "Buzz";
+                }
+
+                if (A == "")
+                {
+                    A += i;
+                }
+
+                Console.WriteLine(A);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
